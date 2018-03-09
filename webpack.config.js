cad5a2fc1c337c245130014c7e4830c26ea34941
ksjogo <jogo@kronberger-spiele.de>
2018-03-09 18:06:48 +0100

    things

--- webpack.config.js
+++ webpack.config.js
@@ -22,20 +22,6 @@ module.exports = {
   module: {
     loaders: [
       {
-<<<<<<< HEAD
-        test: /\.css$/,
-        loader: 'style-loader!css-loader'
-       },
-      {
-      test: /\.tsx?$/,
-      use: [
-        {
-          loader: "awesome-typescript-loader"
-        },
-      ],
-      include: path.join(__dirname, 'src')
-    }]
-=======
         test: /\.tsx?$/,
         use: [
           {
@@ -52,13 +38,8 @@ module.exports = {
         include: path.join(__dirname, 'src')
       }, {
         test: /\.css$/,
-        use: [
-          {
-            loader: "style-loader"
-          },
-          { loader: "css-loader" }
-        ],
-      }]
->>>>>>> 016b46498b4d8e09f74f0893a945338f5a88db51
+        loader: 'style-loader!css-loader'
+      },
+    ]
   }
 };
