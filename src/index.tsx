e3413f7090a15e82c3cbc49359638f143ccce7d8
Vivian Shen <vivian@Vivians-MBP.lan>
2018-03-09 07:48:11 -0500

    added initial map components to react

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,19 @@ import { observable } from 'mobx';
 import { observer } from 'mobx-react';
 import DevTools from 'mobx-react-devtools';
 import AppState from './AppState';
+import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
+
+//Google Maps components
+const MyMapComponent = withScriptjs(withGoogleMap((props) =>
+  <GoogleMap
+    defaultZoom={8}
+    defaultCenter={{ lat: -34.397, lng: 150.644 }}
+  >
+    {props.isMarkerShown && <Marker position={{ lat: -34.397, lng: 150.644 }} />}
+  </GoogleMap>
+))
+<MyMapComponent isMarkerShown />// Map with a Marker
+<MyMapComponent isMarkerShown={false} />// Just only Map
 
 @observer
 class TimerView extends React.Component<{ appState: AppState }, {}> {
