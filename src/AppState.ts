c6c057f59eca2b549c28509e5e2888bfc5c18e64
Vivian Shen <vivian@Vivians-MBP.lan>
2018-03-09 23:40:13 -0500

    some minor fixds

--- src/AppState.ts
+++ src/AppState.ts
@@ -63,6 +63,7 @@ export default class AppState {
     this.recvlat = data.latitude
     this.recvlng = data.longitude
     this.ID = data.markerIdx
+    console.log("ID ",data.markerIdx)
     switch (data.command) {
       case "RemovePoint":
         this.removeP = true
